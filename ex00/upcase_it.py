940dd5a767b71c4f672ac5e32a6dde19439f99cd
rngnnn <rngnnnclk@gmail.com>
2025-07-02 15:14:43 +0200

    first commit

---
+++ ex00/upcase_it.py
@@ -0,0 +1,4 @@
+s="give me a word:"
+s = input("give me a word: ")
+s = s.upper()
+print(s)
---
+++ ex01/age.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+age = input("Please tell me your age: ")
+age2 = int(age) 
+print(f"You are currently {age2} years old")
+print(f"In 10 years, you will be {age2 + 10} years old")
+print(f"In 20 years, you will be {age2 + 20} years old")
+print(f"In 30 years, you will be {age2 + 30} years old")
