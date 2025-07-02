3a11d1ad2df4e72b764400a57fd65d2f8917e8c8
rngnnn <rngnnnclk@gmail.com>
2025-07-02 16:41:05 +0200

    fix: correct input handling and update addition print statement

--- ex02/calculator.py
+++ ex02/calculator.py
@@ -3,7 +3,10 @@ num1=input("Give me the first number: ")
 num2=input("Give me the second number: ")
 num3=input("Thank you! ")
 
-print(num1+num2)
+num1=int(num1)  # Convert the input to an integer
+num2=int(num2)  # Convert the input to an integer
+
+print({num1}+{num2})
 print(num1*num2)
 print(num1-num2)  
 print(num1/num2)
