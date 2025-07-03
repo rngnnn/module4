33250d82abdb7cf4dc78c4f856305325808563aa
rngnnn <rngnnnclk@gmail.com>
2025-07-03 12:15:24 +0200

    feat: add calculator functionality and improve input handling

--- ex02/calculator.py
+++ ex02/calculator.py
@@ -1,12 +1,24 @@
 
-num1=input("Give me the first number: ")
+#!/usr/bin/env python3
+#u satır ************"shebang"********* olarak bilinir.
+#Anlamı: Bu dosya çalıştırıldığında, onu çalıştıracak olan program python3 olsun.
+
+#Terminalde ./calculator.py yazdığında sistem bu satırı okuyarak hangi yorumlayıcıyla çalıştıracağını anlar.
+
+num1=input("Give me the first number: ") #input kullanicidan veri lir  ama bu veri her zaman string yani metin olarak gelir.
+#input fonksiyonu kullanıcının girdiği veriyi alır ve string olarak döner.
+#float ise sayiyi ondalik sayiya cevirir.
+#int ise sayiyi tam sayiya cevirir.
+#input fonksiyonunun içine yazdığımız metin, kullanıcıya gösterilecek mesajdır.
+
+
 num2=input("Give me the second number: ")
-num3=input("Thank you! ")
+print("Thank you! ")
 
 num1=int(num1)  # Convert the input to an integer
-num2=int(num2)  # Convert the input to an integer
+num2=int(num2)  # Convert the input 7to an integer
 
-print({num1}+{num2})
-print(num1*num2)
-print(num1-num2)  
-print(num1/num2)
+print(f"{num1} + {num2} = {num1+num2}")
+print(f"{num1} - {num2} = {num1-num2}")
+print(f"{num1} * {num2} = {num1*num2}")
+print(f"{num1} / {num2} = {num1/num2}")
---
+++ ex03/float.py
@@ -0,0 +1,11 @@
+
+
+num1=input("Give me a number: ")  # input kullanicidan veri lir ama bu veri her zaman string yani metin olarak gelir.
+num1 = float(num1)  # Convert the input string to a float
+if num1.is_integer():
+    # If the number is an integer, convert it to int
+    
+  print(f"The number is an integer: {int(num1)}")
+else:
+  
+    print(f"The number is a decimal: {num1}")  # If it's not an integer, print it as a float
