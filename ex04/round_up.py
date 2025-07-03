88cdf79e45ba4f44fa20b857192abfb1e26c1f15
rngnnn <rngnnnclk@gmail.com>
2025-07-03 13:22:43 +0200

    feat: implement rounding functionality for user input

---
+++ ex04/round_up.py
@@ -0,0 +1,12 @@
+
+num1=input("Give me a number: ")  # input kullanicidan veri lir ama bu veri her zaman string yani metin olarak gelir.
+
+num1 = float(num1)  # Convert the input string to a float
+rounded_num1 = round(num1)  # Round the number to the nearest integer
+print(f"{rounded_num1}")
+
+if num1.is_integer():
+    # If the number is an integer, convert it to int
+    
+  print(int(num1))
+
